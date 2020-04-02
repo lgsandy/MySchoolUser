e7fa5c3682c0a07e6ef475733d461cf9c5b26b96
reactApp_infi <kk36590@gmail.com>
2020-04-02 18:56:50 +0530

    login changes added

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -6,12 +6,7 @@
           <v-layout align-center justify-center>
             <v-flex xs12 sm8 md4>
               <v-flex xs12>
-                <v-img
-                  :src="require('../assets/school.png')"
-                  class="my-3"
-                  contain
-                  height="120"
-                ></v-img>
+                <v-img :src="require('../assets/school.png')" class="my-3" contain height="120"></v-img>
               </v-flex>
               <v-card class="elevation-12">
                 <v-toolbar color="primary" dark flat>
@@ -19,30 +14,38 @@
                   <v-spacer></v-spacer>
                 </v-toolbar>
                 <v-card-text>
-                  <p v-if="showerror" style="color:red;">
-                    Enter Invalid credentials
-                  </p>
+                  <p v-if="showerror" style="color:red;">Enter Invalid credentials</p>
                   <v-form v-model="valid">
                     <v-select
                       prepend-icon="emoji_flags"
-                      :items="items"
-                      label="Select ountry"
+                      :items="country"
+                      v-model="input.country"
+                      label="Select Country"
+                      @change="loaddistrict"
                     ></v-select>
                     <v-select
                       prepend-icon="house"
-                      :items="items"
+                      :items="states"
+                      item-text="name"
                       label="Select State"
+                      v-model="input.state"
+                      @change="loadDist"
                     ></v-select>
                     <v-select
                       prepend-icon="store"
-                      :items="items"
                       label="Select District"
+                      :items="districts"
+                      v-model="input.district"
+                      @change="loadSchool"
                     ></v-select>
                     <v-select
                       prepend-icon="school"
-                      :items="items"
                       label="Select School"
+                      :items="schools"
+                      item-text="id"
+                      v-model="input.school"
                     ></v-select>
+                    <v-text-field label="Mobile No" prepend-icon="phone" v-model="input.mobileNo"></v-text-field>
                   </v-form>
                 </v-card-text>
 
@@ -52,10 +55,10 @@
                   <v-btn color="primary" v-on:click="loginUser()">Login</v-btn>
                 </v-card-actions>
               </v-card>
-              <p align="center">
+              <!-- <p align="center">
                 Don't have account ?
                 <a href="/signup">Sign Up</a>
-              </p>
+              </p>-->
             </v-flex>
           </v-layout>
         </v-container>
@@ -63,15 +66,24 @@
     </v-app>
   </div>
 </template>
+
+
 <script>
 import { mapActions } from "vuex";
 import router from "../router/index";
-
+import countryjson from "../components/countryJson/countrylist.json";
+import { db } from "../components/fireBase/firebaseauth";
 export default {
   name: "Login",
 
   data() {
     return {
+      jsonData: countryjson,
+      country: ["hhh", "jhjh"],
+      states: ["hgg", "jgh"],
+      districts: ["hgg", "jgh"],
+      schools: [],
+      stateIndex: -1,
       showerror: false,
       valid: false,
       show4: false,
@@ -84,36 +96,70 @@ export default {
         v => (v && v.length <= 15) || "password must be less than 15 characters"
       ],
       input: {
-        username: "",
-        password: ""
+        country: "",
+        state: "",
+        district: "",
+        school: "",
+        mobileNo: ""
       }
     };
+  },
+  created() {
+    this.country = Object.keys(this.jsonData);
   },
   methods: {
     ...mapActions(["login"]),
     loginUser() {
-      router.push("/main");
-      //     let user = {
-      //       email: this.input.username,
-      //       password: this.input.password
-      //     };
-      //     this.login(user)
-      //       .then(result => {
+      let ref = db
+        .collection("allschool")
+        .doc(this.input.country)
+        .collection(this.input.state)
+        .doc(this.input.district)
+        .collection(this.input.school)
+        .doc("permission");
+      ref.get().then(res => {
+        if (res && res.data()) {
+          let allDetails = res.data().permission;
+          let matchDetails = allDetails.find(
+            el => el.mobileNumber == this.input.mobileNo
+          );
+          if (matchDetails) {
+            let userDetails = this.input;
+            userDetails.mobileNumber = matchDetails.mobileNumber;
+            userDetails.name = matchDetails.name;
+            console.log("user Data", userDetails);
+            localStorage.userLoginInfo = JSON.stringify(userDetails);
+            router.push("/main");
+          }
+        } else {
+          console.log(res.docs);
+        }
+      });
+    },
+    loaddistrict() {
+      this.states = this.jsonData[this.input.country];
+    },
+    loadDist() {
+      let stateDist = this.states.find(el => el.name == this.input.state);
+      this.districts = stateDist.districts;
 
-      //         console.log(result);
-      //         if (result.data.success) {
-      //  console.log('----------');
-      //         // router.push({name:'/main'}).catch((err) => {console.log(err)})
-      //         this.$router.go();
-
-      //         } else {
-      //           this.showerror = true;
-      //         }
-      //       })
-      //       .catch(err => {
-      //         console.log(err);
-      //          this.showerror = true;
-      //       });
+      // this.stateIndex=this.states.indexOf();
+      // this.districts = this.states.;
+    },
+    loadSchool() {
+      let ref = db
+        .collection("allschool")
+        .doc(this.input.country)
+        .collection(this.input.state)
+        .doc(this.input.district)
+        .collection("school");
+      ref.get().then(res => {
+        if (res && res.docs.length) {
+          this.schools = res.docs;
+        } else {
+          console.log(res.docs);
+        }
+      });
     },
 
     selectedusername() {
@@ -125,3 +171,5 @@ export default {
   }
 };
 </script>
+
+
