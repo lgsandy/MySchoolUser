3a81cc9120a21fdbc4ecb6e2863e21e7c20bf892
unknown <intense-atoll-71797>
2020-04-12 18:11:11 +0530

    Principle Page Completed

--- src/components/principal/principalpage.vue
+++ src/components/principal/principalpage.vue
@@ -6,7 +6,7 @@
           <v-row align="end" class="fill-height">
             <v-col align-self="start" class="pa-0" cols="12">
               <v-avatar class="profile" color="grey" size="164" tile>
-                <v-img src="https://cdn.vuetifyjs.com/images/profiles/marcus.jpg"></v-img>
+                <v-img :src="principal.thumbnail"></v-img>
               </v-avatar>
             </v-col>
             <v-col class="py-0">
