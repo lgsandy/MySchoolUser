20de46455878f9978238933f03b6b0558f3b9f79
unknown <intense-atoll-71797>
2020-04-12 18:06:38 +0530

    Principle Page Completed

--- src/components/principal/principalpage.vue
+++ src/components/principal/principalpage.vue
@@ -1,26 +1,80 @@
 <template>
-    <v-card
-    class="mx-auto"
-    max-width="400"
-  >
-    <v-img
-      class="white--text align-end"
-      height="200px"
-      :src="principal.thumbnail"
-    >
-      <v-card-title>principal</v-card-title>
-    </v-img>
-
-    <v-card-subtitle class="pb-0">Name : {{principal.name}}</v-card-subtitle>
-
-    <v-card-text class="text--primary">
-      <div>Email :{{principal.email}}</div>
-      <div>Phone No. :{{principal.mobileNumber}}</div>
-      <div>Exprience. :{{principal.exprience}} yrs</div>
-       <div>Thought</div>
-        <div>{{principal.thought}} yrs</div>
-    </v-card-text>
-  </v-card>
+  <v-row>
+    <v-col cols="12" sm="6" class="pa-4">
+      <v-card class="mx-auto" max-width="434" tile>
+        <v-img height="100%" src="https://cdn.vuetifyjs.com/images/cards/server-room.jpg">
+          <v-row align="end" class="fill-height">
+            <v-col align-self="start" class="pa-0" cols="12">
+              <v-avatar class="profile" color="grey" size="164" tile>
+                <v-img src="https://cdn.vuetifyjs.com/images/profiles/marcus.jpg"></v-img>
+              </v-avatar>
+            </v-col>
+            <v-col class="py-0">
+              <v-list-item color="rgba(0, 0, 0, .4)" dark>
+                <v-list-item-content>
+                  <v-list-item-title class="title">Marcus Obrien</v-list-item-title>
+                  <v-list-item-subtitle>Network Engineer</v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+            </v-col>
+          </v-row>
+        </v-img>
+        <v-card-actions class="white justify-center">
+          <v-btn
+            v-for="(social, i) in socials"
+            :key="i"
+            :color="social.color"
+            class="mx-3"
+            fab
+            icon
+            outlined
+          >
+            <v-icon large>{{ social.icon }}</v-icon>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+    <v-col cols="12" sm="6" class="pa-4">
+      <v-card class="mx-auto" tile>
+        <v-list rounded>
+          <v-subheader>DETAILS</v-subheader>
+          <v-list-item-group color="primary">
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>email</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title>{{principal.email}}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>phone</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title>{{principal.mobileNumber}}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>explicit</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title>{{principal.exprience}}</v-list-item-title>
+                <v-list-item-title>Year's Experience</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>label_important</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>{{principal.thought}}</v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
@@ -29,7 +83,21 @@ export default {
   inject: ["theme"],
   data: () => ({
     principal: [],
-    loading: false
+    loading: false,
+    socials: [
+      {
+        icon: "fab fa-facebook",
+        color: "indigo"
+      },
+      {
+        icon: "fab fa-linkedin",
+        color: "cyan darken-1"
+      },
+      {
+        icon: "fab fa-instagram",
+        color: "red lighten-3"
+      }
+    ]
   }),
   created() {
     this.loadTeacherData();
