f485a6595962ed04ef79788ef69cf4cd0378432e
Krishnakant Sharma <krishnakant.sharma@semasoft.co.in>
2020-04-12 18:03:43 +0530

    plugin updated

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,3 +1,4 @@
+import "@fortawesome/fontawesome-free/css/all.css";
 import "@mdi/font/css/materialdesignicons.css";
 import Vue from "vue";
 import Vuetify from "vuetify";
@@ -6,6 +7,9 @@ import "vuetify/dist/vuetify.min.css";
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  icons: {
+    iconfont: "md" || "fa"
+  },
   theme: {
     themes: {
       light: {
